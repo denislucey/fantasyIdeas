087d793e253ce957820268cd3bf4a8698ed4ed8a
Denis <LuceyD11702@gmail.com>
2025-06-24 21:34:05 -0400

    got the first model proj working

--- pillar_one/nfl_data_py_test.py
+++ pillar_one/nfl_data_py_test.py
@@ -1,22 +1,68 @@
 import nfl_data_py as nfl
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
 features = [
-    'season','player_id','fantasy_points_ppr'
+    'season','player_id','fantasy_points_ppr','games'
 ]
 
-def main():
+test_features = [
+    'carries', 'rushing_yards',
+    'rushing_tds', 'rushing_fumbles', 'rushing_fumbles_lost',
+    'rushing_first_downs', 'rushing_epa', 'rushing_2pt_conversions',
+    'receptions', 'targets', 'receiving_yards', 'receiving_tds',
+    'receiving_fumbles', 'receiving_fumbles_lost', 'receiving_air_yards',
+    'receiving_yards_after_catch', 'receiving_first_downs', 'receiving_epa',
+    'receiving_2pt_conversions', 'racr', 'target_share', 'air_yards_share',
+    'wopr_x', 'special_teams_tds', 'fantasy_points', 'fantasy_points_ppr',
+    'games', 'tgt_sh', 'ay_sh', 'yac_sh', 'wopr_y', 'ry_sh', 'rtd_sh',
+    'rfd_sh', 'rtdfd_sh', 'dom', 'w8dom', 'yptmpa', 'ppr_sh'
+]
+
+def main() -> str:
+    # creating the dataframe to get player name, id and position
     players = nfl.import_players()
     players = players[['display_name', 'gsis_id','position']]
     players.rename(columns={'display_name': 'full_name', 'gsis_id': 'player_id'}, inplace=True)
     
+    # getting the data from 2015 to 2024
     df = nfl.import_seasonal_data(range(2015, 2025),"REG")
-    slimmed = df[features]
+    print(df.columns)
+
+    slimmed = df
     slimmed = slimmed.merge(players[['player_id', 'full_name', 'position']], on='player_id', how='left')
     rb_df = slimmed[slimmed['position'] == 'RB']
-    print(rb_df.sort_values("fantasy_points_ppr",ascending=False).head(15))
-    
-    # print(rb_df.head(1))
+    print(rb_df.columns)
+    rb_df['fantasy_ppr_ppg'] = rb_df['fantasy_points_ppr'] / rb_df['games']
+    rb_df['next_season_fantasy_points'] = rb_df.groupby('player_id')['fantasy_points_ppr'].shift(-1)
+    rb_df['next_season_fantasy_ppr_ppg'] = rb_df.groupby('player_id')['fantasy_ppr_ppg'].shift(-1)
+    next_season_players = rb_df[rb_df['season'] == 2024]
+    rb_df = rb_df.dropna(subset=['next_season_fantasy_points'])
+    X = rb_df[test_features]
+    y = rb_df['next_season_fantasy_ppr_ppg']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+
+    mae = mean_absolute_error(y_test, predictions)
+    print(f"Model's Average Error: {mae:.2f} fantasy points")
+
+
+    print(next_season_players.head(25))
+    next_season_predictions = model.predict(next_season_players[test_features])
+
+    next_season_players['predicted_fantasy_ppr_ppg'] = next_season_predictions
+    condensed = next_season_players[['full_name', 'player_id', 'predicted_fantasy_ppr_ppg']]
+    print(condensed.sort_values('predicted_fantasy_ppr_ppg',ascending=False).head(25))
+
+
     return "Yes"
 
 
