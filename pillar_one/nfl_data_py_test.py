db75d61b5feb68ce365e1bb93501833909cd0dfc
Denis <LuceyD11702@gmail.com>
2025-06-25 14:03:06 -0400

    Made slightly generic

--- pillar_one/nfl_data_py_test.py
+++ pillar_one/nfl_data_py_test.py
@@ -9,38 +9,30 @@ features = [
     'season','player_id','fantasy_points_ppr','games'
 ]
 
-def project_top_x_players(x: int) -> int:
-    return x
+# creating the dataframe to get player name, id and position
+players = nfl.import_players()
+players = players[['display_name', 'gsis_id','position','birth_date', 'rookie_year']]
+players.rename(columns={'display_name': 'full_name', 'gsis_id': 'player_id'}, inplace=True)
+players = players.dropna(subset=['birth_date'])
+players['birth_year'] = players['birth_date'].str.split('-').str[0].astype(int)
 
-def main() -> str:
-    # creating the dataframe to get player name, id and position
-    players = nfl.import_players()
-    players = players[['display_name', 'gsis_id','position','birth_date', 'rookie_year']]
-    players.rename(columns={'display_name': 'full_name', 'gsis_id': 'player_id'}, inplace=True)
-    players = players.dropna(subset=['birth_date'])
-    players['birth_year'] = players['birth_date'].str.split('-').str[0].astype(int)
-    print(players.columns)
-    
-    # getting the data from 2015 to 2024
-    df = nfl.import_seasonal_data(range(2015, 2025),"REG")
-    # print(df.columns)
 
-    slimmed = df
-    slimmed = slimmed.merge(players[['player_id', 'full_name', 'position','rookie_year','birth_year']], on='player_id', how='left')
-    rb_df = slimmed[slimmed['position'] == 'RB']
-    # print(rb_df.columns)
-    rb_df['fantasy_ppr_ppg'] = rb_df['fantasy_points_ppr'] / rb_df['games']
-    rb_df['years_in_league'] = rb_df['season'] - rb_df['rookie_year']
-    rb_df['age'] = rb_df['season'] - rb_df['birth_year']
-    rb_df['next_season_fantasy_points'] = rb_df.groupby('player_id')['fantasy_points_ppr'].shift(-1)
-    rb_df['next_season_fantasy_ppr_ppg'] = rb_df.groupby('player_id')['fantasy_ppr_ppg'].shift(-1)
-    next_season_players = rb_df[rb_df['season'] == 2024]
-    rb_df = rb_df.dropna(subset=['next_season_fantasy_points'])
-    
-    
-    # ML stuff that I need to better understand
-    X = rb_df[RandomForestFeatures.RB_FEATURES.value]
-    y = rb_df['next_season_fantasy_ppr_ppg']
+df = nfl.import_seasonal_data(range(2015, 2025),"REG")
+merged_df = df.merge(players[['player_id', 'full_name', 'position','rookie_year','birth_year']], on='player_id', how='left')
+
+def project_top_x_players_for_position(x: int, pos: str) -> None:
+
+    pos_df = merged_df[merged_df['position'] == pos]
+    pos_df['fantasy_ppr_ppg'] = pos_df['fantasy_points_ppr'] / pos_df['games']
+    pos_df['years_in_league'] = pos_df['season'] - pos_df['rookie_year']
+    pos_df['age'] = pos_df['season'] - pos_df['birth_year']
+    pos_df['next_season_fantasy_points'] = pos_df.groupby('player_id')['fantasy_points_ppr'].shift(-1)
+    pos_df['next_season_fantasy_ppr_ppg'] = pos_df.groupby('player_id')['fantasy_ppr_ppg'].shift(-1)
+    next_season_players = pos_df[pos_df['season'] == 2024]
+    pos_df = pos_df.dropna(subset=['next_season_fantasy_points'])
+
+    X = pos_df[RandomForestFeatures.RB_FEATURES.value]
+    y = pos_df['next_season_fantasy_ppr_ppg']
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -59,8 +51,19 @@ def main() -> str:
 
     next_season_players['predicted_fantasy_ppr_ppg'] = next_season_predictions * 17
     condensed = next_season_players[['full_name', 'player_id', 'predicted_fantasy_ppr_ppg']]
-    print(condensed.sort_values('predicted_fantasy_ppr_ppg',ascending=False).head(45))
+    
+    print(f"Top {x} projected {pos} for the 2025 season:")
+    print(condensed.sort_values('predicted_fantasy_ppr_ppg',ascending=False).head(x))
 
+    return
+
+
+def main() -> str:
+    # getting the data from 2015 to 2024
+    # ML stuff that I need to better understand
+    project_top_x_players_for_position(45, 'RB')
+    project_top_x_players_for_position(45, 'WR')
+    project_top_x_players_for_position(25, 'TE')
     return "Yes"
 
 
